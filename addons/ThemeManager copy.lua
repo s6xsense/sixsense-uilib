6514a48e89fc28ff127155c2446e2f078d84408c
xXehub <artixsanni@gmail.com>
2025-12-07 23:41:20 +0700

    fix: Update default theme colors in ThemeManager for improved aesthetics

--- addons/ThemeManager copy.lua
+++ addons/ThemeManager copy.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "191919", AccentColor = "8bff55", BackgroundColor = "1d1d1d", OutlineColor = "282828" },
+            { FontColor = "ffffff", MainColor = "191919", AccentColor = "7d55ff", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
         },
         ["BBot"] = {
             2,
